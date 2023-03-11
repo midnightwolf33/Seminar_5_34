4e19c867ab6a440e3cc7cf9bf6411a3d50860064
midnight33 <gra33@bk.ru>
2023-03-11 13:42:53 +0300

    Задача 34

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿// Урок 5. Функции и одномерные массивы
+
+
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] DArray(int len, int minValue, int maxValue)
+{
+    int[] arr = new int[len];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return arr;
+}
+
+void PrintDArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+
+void BubleSort (int[] arr)
+{
+    int temp = 0;
+        for (int i = 0; i < arr.Length-1; i++)
+    {   
+        for (int j = 0; j < arr.Length - 1-i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+
+        }
+    }
+    
+}
+
+int[] GArr = DArray(10,100,999);
+
+PrintDArr(GArr);
+BubleSort(GArr);
+PrintDArr(GArr);
